dd9807e5d18f1e52ad85e5f6c70a03ab059ec46c
seniordev32 <senior.dev32@gmail.com>
2018-11-22 00:19:20 +0000

    minor fix

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -45,8 +45,8 @@ const std::string CLIENT_NAME("Satoshi");
 //! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "9b3d04e"
-#define GIT_COMMIT_DATE "Tue, 9 Jan 2018 07:41:17 +0100"
+#define GIT_COMMIT_ID "660ff85"
+#define GIT_COMMIT_DATE "Tue, 9 Jan 2018 09:14:57 -0500"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
