18869b261bc8286752323c87519e9c81ea18e26b
seniordev32 <senior.dev32@gmail.com>
2018-11-26 15:12:42 +0000

    Remove optional burn

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -45,8 +45,8 @@ const std::string CLIENT_NAME("Satoshi");
 //! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "64bea8b"
-#define GIT_COMMIT_DATE "Wed, 10 Jan 2018 16:47:52 -0300"
+#define GIT_COMMIT_ID "e0fe199"
+#define GIT_COMMIT_DATE "Wed, 10 Jan 2018 23:10:48 +0100"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
