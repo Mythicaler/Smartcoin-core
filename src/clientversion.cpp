fcfeeadc4849f9459b649b787c251f6053be2ed6
seniordev32 <senior.dev32@gmail.com>
2018-11-27 12:21:10 +0000

    smartnode and core upgrade

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -45,8 +45,8 @@ const std::string CLIENT_NAME("Satoshi");
 //! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "531309a"
-#define GIT_COMMIT_DATE "Sun, 21 Jan 2018 19:42:59 -0300"
+#define GIT_COMMIT_ID "ee48f12"
+#define GIT_COMMIT_DATE "Tue, 23 Jan 2018 02:01:52 -0300"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
