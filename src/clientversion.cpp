a22c9b9d66edbdcbb42a0e2902ae95a84360e61e
seniordev32 <senior.dev32@gmail.com>
2018-11-28 09:14:23 +0000

    Add smartash pixmap

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -45,8 +45,8 @@ const std::string CLIENT_NAME("Satoshi");
 //! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "2adf5d0"
-#define GIT_COMMIT_DATE "Thu, 25 Jan 2018 02:31:12 -0300"
+#define GIT_COMMIT_ID "061014b"
+#define GIT_COMMIT_DATE "Sat, 27 Jan 2018 15:30:26 -0500"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
