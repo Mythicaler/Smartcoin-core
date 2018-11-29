bc1682a7b48ff23f3879c3d2ae34adc64881e0db
seniordev32 <senior.dev32@gmail.com>
2018-11-29 08:53:20 +0000

    smartcashfy and upgrade init

--- src/bip39.h
+++ src/bip39.h
@@ -21,8 +21,8 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#ifndef DASH_BIP39_H
-#define DASH_BIP39_H
+#ifndef smartcoin_BIP39_H
+#define smartcoin_BIP39_H
 
 #include "support/allocators/secure.h"
 
