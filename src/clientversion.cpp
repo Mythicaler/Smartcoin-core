202b3a422b5b0c9baf3804ed77abdbe14460dce4
seniordev32 <senior.dev32@gmail.com>
2018-11-30 14:48:16 +0000

    adding button tools function and reviewing protocol version

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -45,8 +45,8 @@ const std::string CLIENT_NAME("smartcoin Core");
 //! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "bd373f4"
-#define GIT_COMMIT_DATE "Thu, 1 Mar 2018 22:14:37 -0300"
+#define GIT_COMMIT_ID "122ebc5"
+#define GIT_COMMIT_DATE "Thu, 1 Mar 2018 22:46:03 -0300"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
