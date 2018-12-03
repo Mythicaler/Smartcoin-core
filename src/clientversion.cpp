b71825c9501a7782b146d613cf1cc0ea0a22da8a
seniordev32 <senior.dev32@gmail.com>
2018-12-03 09:09:17 +0000

    rpc calls improvement

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -45,8 +45,8 @@ const std::string CLIENT_NAME("smartcoin Core");
 //! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "6e52b4a"
-#define GIT_COMMIT_DATE "Sun, 18 Mar 2018 21:03:10 -0300"
+#define GIT_COMMIT_ID "7cd8151"
+#define GIT_COMMIT_DATE "Thu, 22 Mar 2018 21:09:56 -0300"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
